13e626b8215bbe9bdf8ffbaab0f2c65ff6f1d3d6
eduardaraujo <mpmaeduarda2002@gmail.com>
2022-10-12 23:13:44 +0100

    Application is done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,16 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
     }
 }
--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -5,9 +5,38 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 
+import javax.swing.text.Position;
 import java.awt.*;
 import java.io.IOException;
 
 public class Hero {
-    //Hero hero = new Hero(10, 10);
+    //private int x;
+    //private int y;
+    //private Position position;
+
+    public Hero (Position position) {
+        super(position);
+    }
+
+    public Position moveUp() {
+        return new Position(getX(), getY() - 1);
+    }
+
+    public Position moveDown() {
+        return new Position() {getX(), getY() + 1};
+    }
+
+    public  Position moveLeft() {
+        return new Position(getX() - 1, getY());
+    }
+
+    public Position moveRight() {
+        return new Position(getX() + 1, getY());
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#0000FF"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(getX(), getY(), "X"));
+    }
 }
